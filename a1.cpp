5f7abbbb2448c951ae7f1dc5b20f5e99d9322700
Song Yang <songyang1218@gmail.com>
2019-02-11 11:33:21 -0500

    Q2

--- a1.cpp
+++ a1.cpp
@@ -26,7 +26,99 @@ void countLetter() {
 		<< count << " times in your sentence" << endl;
 }
 
+void convertPhonetic() {
+	cout << "Please enter a word: ";
+	string input;
+	cin >> input;
+
+	for (int i = 0; i < input.length(); i++) {
+		switch (tolower(input[i])) {
+		case 'a':
+			cout << "Alfa";
+			break;
+		case 'b':
+			cout << "Bravo";
+			break;
+		case 'c':
+			cout << "Charlie";
+			break;
+		case 'd':
+			cout << "Delta";
+			break;
+		case 'e':
+			cout << "Echo";
+			break;
+		case 'f':
+			cout << "Foxtrot";
+			break;
+		case 'g':
+			cout << "Golf";
+			break;
+		case 'h':
+			cout << "Hotel";
+			break;
+		case 'i':
+			cout << "India";
+			break;
+		case 'j':
+			cout << "Juliett";
+			break;
+		case 'k':
+			cout << "Kilo";
+			break;
+		case 'l':
+			cout << "Lima";
+			break;
+		case 'm':
+			cout << "Mike";
+			break;
+		case 'n':
+			cout << "November";
+			break;
+		case 'o':
+			cout << "Oscar";
+			break;
+		case 'p':
+			cout << "Papa";
+			break;
+		case 'q':
+			cout << "Quebec";
+			break;
+		case 'r':
+			cout << "Romeo";
+			break;
+		case 's':
+			cout << "Sierra";
+			break;
+		case 't':
+			cout << "Tango";
+			break;
+		case 'u':
+			cout << "Uniform";
+			break;
+		case 'v':
+			cout << "Victor";
+			break;
+		case 'w':
+			cout << "Whiskey";
+			break;
+		case 'x':
+			cout << "X-ray";
+			break;
+		case 'y':
+			cout << "Yankee";
+			break;
+		case 'z':
+			cout << "Zulu";
+			break;
+		}
+
+		cout << " ";
+	}
+}
+
 int main()
 {
 	countLetter();
+	convertPhonetic();
 }
